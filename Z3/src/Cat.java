ed9da777b9f9267359c4381c913f0a0167036394
Sturvi <114976816+Sturvi@users.noreply.github.com>
2022-11-29 19:10:11 +0400

    Z 3, 4, 5

---
+++ Z3/src/Cat.java
@@ -0,0 +1,16 @@
+public class Cat implements Eat, Movable, Edible{
+    @Override
+    public void beEaten() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+}
---
+++ Z4/src/Car.java
@@ -0,0 +1,27 @@
+public class Car {
+    String carModel;
+
+    public Car(String carModel) {
+        this.carModel = carModel;
+    }
+}
+
+
+public class Engine {
+    boolean isWorking = false;
+    int horsepower;
+
+
+    public Engine(int horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    String startStopEngine(){
+        if (isWorking) {
+            isWorking = false;
+            return "engine was stopped";
+        }
+        isWorking = true;
+        return "engine was started";
+    }
+}
